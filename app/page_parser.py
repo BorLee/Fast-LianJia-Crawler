2ea5b3da754cf8443301fa975eaeb548a7e47c60
BorLee <borlee2567@gmail.com>
2018-03-15 10:54:01 +0800

    增加对城市的识别
    上海和苏州的页面与其他城市不抓取detail前需区分。还有其他城市也有特殊页面，未处理。

--- app/page_parser.py
+++ app/page_parser.py
@@ -7,18 +7,41 @@ from config import config
 from lian_jia import Community
 from util.orm import Session
 
+from bs4 import BeautifulSoup
+
 DATA_DIR = Path(__file__).parent.joinpath('../data/').resolve()
 
+def removehtml(context):
+    import re
+    context = re.sub(r'</?\w+[^>]*>','',context)
+    context = context.replace(' ','')
+    context = context.replace('\n','')
+    return context
 
-def parse_community_detail(community_id):
+def parse_community_detail(community_id,city_id):
     save_file = DATA_DIR.joinpath(f'{community_id}.html')
 
     if save_file.exists():
-        d = pq(save_file.read_text(encoding='utf-8'))
+        if city_id == 310000 or city_id == 320500:
+            context = BeautifulSoup(save_file.read_text(encoding='utf-8'),'lxml')
+            context = BeautifulSoup(context.prettify(),'lxml')
+            context = context.find_all(attrs={'class':'res-info fr'})[0]
+            context = BeautifulSoup(str(context),'lxml')
 
-        keys = [e.text.rstrip('：') for e in d('span.hdic_key')]
-        values = [e.text for e in d('span.hdic_value')]
-        detail = dict(zip(keys, values))
+            fkeys = str(context.find(attrs={'class':'t'}))
+            fvalues = str(context.find(attrs={'class':'p'})) + str(context.find(attrs={'class':'u'}))
+
+            keys = [removehtml(str(e)).rstrip('：') for e in context.find_all('label')]
+            values = [removehtml(str(e)) for e in context.find_all(attrs={'class':'other'})]
+
+            keys = [removehtml(fkeys)] + keys
+            values = [removehtml(fvalues)] + values
+            detail = dict(zip(keys, values))
+        else:
+            d = pq(save_file.read_text(encoding='utf-8'))
+            keys = [e.text.rstrip('：') for e in d('span.hdic_key')]
+            values = [e.text for e in d('span.hdic_value')]
+            detail = dict(zip(keys, values))
 
         if not detail:
             logging.warning(f'# 页面已找到, 但其中未发现小区详情信息, community_id={community_id}')
@@ -42,7 +65,7 @@ def parse_all_communities(city_id):
     logging.info(f'city_id={city_id}, 待分析={total_count}')
 
     for i, a_community in enumerate(communities):
-        detail = parse_community_detail(a_community.id)
+        detail = parse_community_detail(a_community.id,city_id)
 
         if detail:
             a_community.detail = detail
